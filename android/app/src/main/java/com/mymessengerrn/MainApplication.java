316707883fd66fb65b74ccc7e7aa5640768a127b
huynhtnhut <luminous997@gmail.com>
2018-10-17 15:17:57 +0700

    temp disable FBSDK to make app runnable

--- android/app/src/main/java/com/mymessengerrn/MainApplication.java
+++ android/app/src/main/java/com/mymessengerrn/MainApplication.java
@@ -36,7 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
-            new FBSDKPackage(),
+              //new FBSDKPackage(),
               new RNFirebasePackage(),
               new RNGoogleSigninPackage(),
               new FBSDKPackage(mCallbackManager),
