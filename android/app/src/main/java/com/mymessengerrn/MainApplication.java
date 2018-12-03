ae3406ec58e86a99032f3d9ba729338eb5ff3bd8
Hoàng Khánh <khanh.ht1996@gmail.com>
2018-12-03 20:19:55 +0700

    Setup react-native-elements

--- android/app/src/main/java/com/mymessengerrn/MainApplication.java
+++ android/app/src/main/java/com/mymessengerrn/MainApplication.java
@@ -3,6 +3,7 @@ package com.mymessengerrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import io.invertase.firebase.RNFirebasePackage;
@@ -37,6 +38,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
+            new VectorIconsPackage(),
               //new FBSDKPackage(),
               new RNFirebasePackage(),
               new RNGoogleSigninPackage(),
