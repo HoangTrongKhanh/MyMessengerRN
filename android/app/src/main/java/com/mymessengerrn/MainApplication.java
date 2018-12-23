5b325732231dc2c03ff7c7c768ca26f733194b7b
Hoàng Khánh <khanh.ht1996@gmail.com>
2018-12-23 20:24:28 +0700

    Add ImagePicker

--- android/app/src/main/java/com/mymessengerrn/MainApplication.java
+++ android/app/src/main/java/com/mymessengerrn/MainApplication.java
@@ -3,6 +3,7 @@ package com.mymessengerrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
@@ -38,6 +39,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
+            new ImagePickerPackage(),
             new VectorIconsPackage(),
               //new FBSDKPackage(),
               new RNFirebasePackage(),
